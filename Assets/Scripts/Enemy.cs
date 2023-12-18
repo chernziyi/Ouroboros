653ac36bb7086c1e98df8818b5e87352dcdfa3b2
HyperMuffin <75190658+chernziyi@users.noreply.github.com>
2023-12-18 16:15:32 +0800

    Add files via upload

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float health;
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+            Debug.Log("KO!");
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+
+    float timeBtwAttacks;
+    [SerializeField] float weaponUsed;
+
+    public int Weapon1;
+    [SerializeField] private float attackSpeed1;
+    [SerializeField] private float weaponDamage1;
+
+    public int Weapon2;
+    [SerializeField] private float attackSpeed2;
+    [SerializeField] private float weaponDamage2;
+
+    private void Start()
+    {
+        Weapon1 = 1; //1 is fist, remind to make readme
+        Weapon2 = 1;
+    }
+
+    private void Update()
+    {
+
+
+        if (timeBtwAttacks <= 0)
+        {
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                anim.SetInteger("Weapon", Weapon1);
+                timeBtwAttacks = attackSpeed1;
+                weaponUsed = 1;
+
+            } else if (Input.GetMouseButtonDown(1))
+            {
+                anim.SetInteger("Weapon", Weapon2);
+                timeBtwAttacks = attackSpeed2;
+                weaponUsed = 2;
+            }
+        } else
+        {
+            anim.SetInteger("Weapon", 0); //to actually trigger the damn thing like i swear to fucking god
+            timeBtwAttacks -= Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Enemy")
+        {
+            if(weaponUsed == 1)
+            {
+                other.GetComponent<Enemy>().TakeDamage(weaponDamage1);
+            }else if(weaponUsed == 2)
+            {
+                other.GetComponent<Enemy>().TakeDamage(weaponDamage2);
+            }
+            Debug.Log("Strike!");
+        }
+    }
+
+}
+
+    
