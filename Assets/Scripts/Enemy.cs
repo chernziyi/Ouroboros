960659910ac88fed75fa10759d81d3a29a711e75
Chern Zi Yi <ziyi.chern@gmail.com>
2023-12-19 23:36:14 +0800

    spawn, souls and cam follow

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,12 +6,19 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] private float health;
 
+    public float soulDrop;
+    [SerializeField] GameObject soul;
+
     public void TakeDamage(float damage)
     {
         health -= damage;
 
         if(health <= 0)
         {
+            for(int i = 0; i < soulDrop; i++)
+            {
+                Instantiate(soul, transform.position, Quaternion.identity);
+            }
             Destroy(gameObject);
             Debug.Log("KO!");
         }
